e361bf888486a51ccee72acd8230b5ab857481b3
Micah Denbraver <macromicah@gmail.com>
2014-07-30 21:19:40 -0700

    Update 2014-07-31T04:19:40.962Z

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,106 @@
-angular.module("search",["ngSanitize"]),angular.module("search").controller("SearchController",["$scope","$filter","$http",function(e,r,t){return e.result=[],e.category="sss",e.errors=[],t.get("data/cities.json").success(function(r){return e.cities=r}),e.search=function(r){return e.errors.length=0,e.result.length=0,e.query=r,e.load(),$("#city_list").hide()},e.load=function(){var t,s,n,o,u,a,i,l;for(t=function(r){return function(t){var s,n,o,u,a;if(n=void 0,r.status="processing",t.error)return e.errors.push(r.name),r.status="error";for(a=t.feed.entries,o=0,u=a.length;u>o;o++)s=a[o],s.publishedDate=Date.parse(s.publishedDate);return e.result=e.result.concat(t.feed.entries),r.status="success",e.$apply()}},u=void 0,o=void 0,s=r("filter")(e.cities,{selected:!0}),e.result.length=0,l=[],a=0,i=s.length;i>a;a++)n=s[a],o=new google.feeds.Feed("http://"+n.slug+".craigslist.org/search/?catAbb="+e.category+"&query="+e.query+"&format=rss"),n.status="pending",o.setNumEntries(100),l.push(o.load(t(n)));return l},e.citiesto=function(r){var t,s;for(t=void 0,t=0,s=[];t<e.cities.length;)e.cities[t].selected=r,s.push(t+=1);return s},e.now=function(){return new Date}}]),angular.module("search").filter("delta",function(){return function(e){var r,t,s,n;return n=Math.floor(e/1e3),s=Math.floor(n/60),t=Math.floor(s/60),r=Math.floor(t/24),r>0?r+"d "+(t-24*r)+"h":t>0?t+"h "+(s-60*t)+"m":s>0?s+"m "+(n-60*s)+"s":n+"s"}});
+angular.module('search', ['ngSanitize']);
+
+angular.module("search").controller("SearchController", [
+  '$scope', '$filter', '$http', 'cities', function($scope, $filter, $http, cities) {
+    $scope.result = [];
+    $scope.category = "sss";
+    $scope.errors = [];
+    $scope.cities = cities;
+    $scope.search = function(query) {
+      $scope.errors.length = 0;
+      $scope.result.length = 0;
+      $scope.query = query;
+      $scope.load();
+      return $("#city_list").hide();
+    };
+    $scope.load = function() {
+      var callback, city, feed, i, _i, _len, _ref, _results;
+      callback = function(city) {
+        return function(result) {
+          var entry, i, _i, _len, _ref;
+          i = void 0;
+          city.status = "processing";
+          if (!result.error) {
+            _ref = result.feed.entries;
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              entry = _ref[_i];
+              entry.publishedDate = Date.parse(entry.publishedDate);
+            }
+            $scope.result = $scope.result.concat(result.feed.entries);
+            city.status = "success";
+            return $scope.$apply();
+          } else {
+            $scope.errors.push(city.name);
+            return city.status = "error";
+          }
+        };
+      };
+      i = void 0;
+      feed = void 0;
+      $scope.result.length = 0;
+      _ref = $scope.cities.selected();
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        city = _ref[_i];
+        feed = new google.feeds.Feed("http://" + city.slug + ".craigslist.org/search/?catAbb=" + $scope.category + "&query=" + $scope.query + "&format=rss");
+        city.status = "pending";
+        feed.setNumEntries(100);
+        _results.push(feed.load(callback(city)));
+      }
+      return _results;
+    };
+    $scope.citiesto = function(state) {
+      var city, _i, _len, _ref, _results;
+      _ref = $scope.cities.all;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        city = _ref[_i];
+        _results.push(city.selected = state);
+      }
+      return _results;
+    };
+    return $scope.now = function() {
+      return new Date();
+    };
+  }
+]);
+
+
+
+angular.module("search").filter("delta", function() {
+  return function(delta) {
+    var days, hours, minutes, seconds;
+    seconds = Math.floor(delta / 1000);
+    minutes = Math.floor(seconds / 60);
+    hours = Math.floor(minutes / 60);
+    days = Math.floor(hours / 24);
+    if (days > 0) {
+      return days + "d " + (hours - days * 24) + "h";
+    }
+    if (hours > 0) {
+      return hours + "h " + (minutes - hours * 60) + "m";
+    }
+    if (minutes > 0) {
+      return minutes + "m " + (seconds - minutes * 60) + "s";
+    }
+    return seconds + "s";
+  };
+});
+
+angular.module("search").factory("cities", [
+  "$http", "$filter", "$q", function($http, $filter, $q) {
+    var all;
+    all = [];
+    $http.get('data/cities.json').success(function(data) {
+      return angular.copy(data, all);
+    });
+    return {
+      all: all,
+      selected: function() {
+        return $filter("filter")(this.all, {
+          selected: true
+        });
+      }
+    };
+  }
+]);
